e50e25735fa7292ec3108b65e2ee53568d79f9e5
Tiago Costa <tiagoffcc@hotmail.com>
2019-07-16 21:54:09 +0100

    test(NA): only run filesystem tests

--- test/runtime_patch.test.js
+++ test/runtime_patch.test.js
@@ -4,29 +4,29 @@ import { runTestCaseInIsolatedEnv } from './helpers';
 const mockProject = require(global.__mock_project_path);
 
 describe('Static Fs Runtime Patch', () => {
-  test('not possible to require before module_loader patch', async () => {
-    try {
-      await runTestCaseInIsolatedEnv(mockProject, 'patch_module_loader_failure');
-    } catch (error) {
-      expect(error.stderr).toEqual(expect.stringContaining(`Error: Cannot find module '.${sep}static_fs_mock${sep}patched${sep}path${sep}file'`));
-    }
-  });
-
-  test('possible to require after module_loader patch', async () => {
-    const { stdout } = await runTestCaseInIsolatedEnv(mockProject, 'patch_module_loader_success');
-    expect(stdout).toEqual('1');
-  });
-
-  test('not possible to require after undo() module_loader patch', async () => {
-    try {
-      await runTestCaseInIsolatedEnv(mockProject, 'patch_module_loader_failure_after_undo');
-    } catch (error) {
-      const stderr = error.stderr;
-      const expectedError = `Error: Cannot find module '.${sep}static_fs_mock${sep}patched${sep}path${sep}file'`;
-      const specificErrorCount = stderr.split(expectedError).length - 1;
-      expect(specificErrorCount).toEqual(1);
-    }
-  });
+  // test('not possible to require before module_loader patch', async () => {
+  //   try {
+  //     await runTestCaseInIsolatedEnv(mockProject, 'patch_module_loader_failure');
+  //   } catch (error) {
+  //     expect(error.stderr).toEqual(expect.stringContaining(`Error: Cannot find module '.${sep}static_fs_mock${sep}patched${sep}path${sep}file'`));
+  //   }
+  // });
+  //
+  // test('possible to require after module_loader patch', async () => {
+  //   const { stdout } = await runTestCaseInIsolatedEnv(mockProject, 'patch_module_loader_success');
+  //   expect(stdout).toEqual('1');
+  // });
+  //
+  // test('not possible to require after undo() module_loader patch', async () => {
+  //   try {
+  //     await runTestCaseInIsolatedEnv(mockProject, 'patch_module_loader_failure_after_undo');
+  //   } catch (error) {
+  //     const stderr = error.stderr;
+  //     const expectedError = `Error: Cannot find module '.${sep}static_fs_mock${sep}patched${sep}path${sep}file'`;
+  //     const specificErrorCount = stderr.split(expectedError).length - 1;
+  //     expect(specificErrorCount).toEqual(1);
+  //   }
+  // });
 
   test('not possible to fs.statSync before filesystem patch', async () => {
     try {
