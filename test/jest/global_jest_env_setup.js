2fbaba46a29f7ebd2b33cb933b2ee95cb913febd
Tiago Costa <tiagoffcc@hotmail.com>
2019-07-27 17:21:16 +0100

    chore: merge and solve conflicts

--- test/jest/global_jest_env_setup.js
+++ test/jest/global_jest_env_setup.js
@@ -1,6 +1,6 @@
 /* global jest */
 // sets global async timeout
-jest.setTimeout(120000);
+jest.setTimeout(60000);
 
 // get global mock project path and set it to a global
 global.__mock_project_path = process.env.GLOBAL_MOCK_PROJECT_PATH;
