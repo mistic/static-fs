9fe100d87570234d7abafdda8d6df5b410f795d9
Tiago Costa <tiagoffcc@hotmail.com>
2019-08-21 19:17:16 +0100

    chore: fix semantic commit dependency plugin delcaration

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   branch: 'master',
   plugins: [
-    'semantic-release/commit-analyzer',
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', {
       changelogTitle: 'static-fs Changelog',
