e55baa9de3a659c99584e80fa2e2a1da5bba0f53
Tiago Costa <tiagoffcc@hotmail.com>
2020-02-14 03:57:27 +0000

    chore: adjust max header between local and ci merge on master

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   extends: [
     '@commitlint/config-conventional'
-  ]
+  ],
+  rules: {
+    // This was changed to always show the entire message on github
+    // Default was 100
+    'header-max-length': [2, 'always', 64],
+  }
 };
