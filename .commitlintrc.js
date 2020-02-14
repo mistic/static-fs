2ea0a1b200a3dc6e8d9497ad02b589503664eb43
Tiago Costa <tiagoffcc@hotmail.com>
2020-02-14 05:18:01 +0000

    chore: increase max header length for commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,8 +3,8 @@ module.exports = {
     '@commitlint/config-conventional'
   ],
   rules: {
-    // This was changed to always show the entire message on github
-    // Default was 100
-    'header-max-length': [2, 'always', 64],
+    // This was changed to have a bigger message on github
+    // Default was 72
+    'header-max-length': [2, 'always', 100],
   }
 };
