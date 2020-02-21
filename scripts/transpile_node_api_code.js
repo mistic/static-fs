361f3796643c7c70ce774822d0d76add9964b506
Tiago Costa <tiagoffcc@hotmail.com>
2020-02-21 23:42:16 +0000

    fix: update deps causing random errors

--- scripts/transpile_node_api_code.js
+++ scripts/transpile_node_api_code.js
@@ -7,7 +7,7 @@ const execa = require('execa');
     //
     // Exclude the runtime code as we'll want to generate a single runtime file.
     console.log('Transpiling node api code');
-    const { stdout } = await execa.shell('babel src --out-dir dist --ignore src/runtime');
+    const { stdout } = await execa.command('babel src --out-dir dist --ignore src/runtime');
     console.log(stdout);
     console.log('Transpilation of node api code end');
   } catch (error) {
--- test/jest/global_setup.js
+++ test/jest/global_setup.js
@@ -15,8 +15,8 @@ module.exports = async () => {
     process.env.JOBS = Math.max(1, os.cpus().length -1 );
 
     await rimRafAsync(resolve('test/helpers/mock_project/node_modules'), { glob: false });
-    execa.shellSync('yarn install --no-lockfile', { cwd: 'test/helpers/mock_project' });
-    execa.shellSync('yarn build', { cwd: 'test/helpers/mock_project/node_modules/static-fs' });
+    execa.commandSync('yarn install --no-lockfile', { cwd: 'test/helpers/mock_project' });
+    execa.commandSync('yarn build', { cwd: 'test/helpers/mock_project/node_modules/static-fs' });
 
     process.env.GLOBAL_MOCK_PROJECT_PATH = await createTestTempDir();
   } catch (error) {
